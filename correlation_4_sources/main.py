2acee47f5fbffa449ec221df4d3ce8dac27c2c00
Justin Vallée <62106699+JustinVallee@users.noreply.github.com>
2024-05-20 13:51:57 -0400

    Initial Commit

---
+++ correlation_4_sources/main.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib_venn import venn2, venn2_circles, venn3, venn3_circles
+
+df = pd.read_csv('datasets/cves_2021_exploited.csv')
+#graynoise_reports_count, clam, secureworks,cisa
+
+# Initialize a counter variable
+countGraynoise = 0
+countClam = 0
+countSecureworks = 0
+countCisa = 0
+
+countABC = 0
+countClamSecureworks = 0
+countClamCisa = 0
+countSecureworksCisa = 0
+
+# Iterate over each row in the DataFrame
+for index, row in df.iterrows():
+    # Check if the value in the 'cisa' column is 1
+    if row['graynoise_reports_count'] != 0:
+        # Increment the counter
+        countGraynoise += 1
+    if row['clam'] != 0:
+        # Increment the counter
+        countClam += 1
+    if row['secureworks'] != 0:
+        # Increment the counter
+        countSecureworks += 1
+    if row['cisa'] != 0:
+        # Increment the counter
+        countCisa += 1
+
+    # CorrelationsABC
+    if row['clam'] and row['secureworks'] and row['cisa'] !=0:
+        countABC +=1
+    # Correlations CountClamSecureworks
+    if (row['clam'] and row['secureworks'] !=0) and (row['cisa'] == 0):
+        countClamSecureworks +=1
+    # CorrelationsAC
+    if (row['clam'] and row['cisa'] !=0) and (row['secureworks'] == 0):
+        countClamCisa +=1
+    # CorrelationsBC
+    if (row['secureworks'] and row['cisa'] !=0) and (row['clam'] == 0):
+        countSecureworksCisa +=1
+
+
+
+print("countGraynoise: " + str(countGraynoise))
+print("countClam: " + str(countClam))
+print("countSecureworks: " + str(countSecureworks))
+print("countCisa: " + str(countCisa))
+
+print("\ncountABC: " + str(countABC))
+print("countClamSecureworks: " + str(countClamSecureworks))
+print("countClamCisa: " + str(countClamCisa))
+print("countSecureworksCisa: " + str(countSecureworksCisa))
+
+# Create Venn diagram
+plt.figure()
+venn3(subsets=(countClam-countABC-countClamSecureworks-countClamCisa,countSecureworks-countABC-countClamSecureworks-countSecureworksCisa,countClamSecureworks,countCisa-countABC-countClamCisa-countSecureworksCisa,countClamCisa,countSecureworksCisa,countABC), set_labels=('Clam', 'Secureworks', 'Cisa'))
+plt.title("Venn Diagram of Common Non-Zero Values")
+plt.show()
